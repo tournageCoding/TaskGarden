5df6e80a7cff75c28b11e9801d207db5592d2be2
JacobGear <imjacobgear@gmail.com>
2021-08-13 13:31:56 +1200

    Created a test suite for CompletedTasksActivity class. No tests working yet.

---
+++ app/src/test/java/com/garden/taskgarden/CompletedTasksActivityTest.kt
@@ -0,0 +1,15 @@
+package com.garden.taskgarden
+
+import junit.framework.TestCase
+import org.junit.Assert
+
+class CompletedTasksActivityTest {
+
+    /* Tests if loaded tasks are marked as completed or failed
+    *   1 = completed task
+    *   2 = failed task
+    * */
+    fun testLoadCompletedTasks() {
+
+    }
+}
